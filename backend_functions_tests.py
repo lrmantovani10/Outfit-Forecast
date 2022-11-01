21531dc3e57472a51fc17f40424d529bbf29f25c
grkapoor17 <grkapoor@uchicago.edu>
2022-11-01 00:37:31 -0500

    Added User class and test-preferences function

--- backend_functions_tests.py
+++ backend_functions_tests.py
@@ -1,8 +1,8 @@
 import unittest
-import backend_functions
+from backend_functions import *
 
 class TestUser(unittest.TestCase):
-    # currentUser = User("asdasda88>>?")
+    newUser = User(" ")
 
     def test_username(self):
         self.assertFalse(self.set_username("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklm"), "Username is more than 32 characters") 
@@ -15,6 +15,16 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.get_username(), "abcde12")
         self.assertFalse(self.set_username("abcdefg123!!"), "Username cannot have special characters") 
 
+    def test_preferences(self):
+        self.assertFalse(self.set_preference([-20, 70], "Temperature cannot be below -20 Farenheit")) 
+        self.assertFalse(self.set_preference([0, 130], "Temperature cannot be above 120 Farenheit"))
+        self.assertTrue(self.set_preference([-10, 120])) # Lower and upper bound included
+        self.assertEqual(self.get_preferences(), [[-10, 120]])
+        self.assertTrue(self.set_preference([0, 65]))
+        self.assertEqual(self.get_preferences(), [[-10, 120], [0, 65]])
+        self.assertFalse(self.set_preference([130], "Preference must be an array of 2 elements"))
+        self.assertFalse(self.set_preference([], "Preference must be an array of 2 elements"))
+        
 
 if __name__ == '__main__':
         unittest.main() 
