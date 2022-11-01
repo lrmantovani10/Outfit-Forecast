fbbdd58fab2ea16a701e14ea9fb2935dc5e3089f
perenewang <perenewang@users.noreply.github.com>
2022-11-01 16:52:08 -0500

    added test functions for classifyNew and for Clothing class

--- backend_functions.py
+++ backend_functions.py
@@ -1,3 +1,43 @@
 class User:
   def __init__(self, username):
-    self.usernamename = username
+    self.username = username
+    self.wardrobe = []
+    self.clothingHistory = []
+
+  def get_username(self):
+      return self.username
+
+class Clothing:
+  def __init__(self, name, imgURL, clothingID):
+    self.objectName = name
+    self.warmthRating = 0
+    self.clothingID = clothingID
+    self.classification = ""
+    self.imgURL = imgURL
+
+  # getters
+  def get_objectName(self):
+    return self.objectName
+
+  def get_warmthRating(self):
+      return self.warmthRating
+
+  def get_clothingID(self):
+      return self.clothingID
+  
+  def get_classification(self):
+      return self.classification
+
+  def get_imgURL(self):
+      return self.imgURL
+  
+  # setters
+  def set_objectName(self):
+      return True
+
+  def set_warmthRating(self):
+      return True
+  
+  def set_classification(self):
+      return True
+
--- backend_functions_tests.py
+++ backend_functions_tests.py
@@ -38,5 +38,46 @@ class TestUser(unittest.TestCase):
         self.assertTrue(newUser.set_location([50, 65]))
         self.assertEqual(newUser.get_location(), [50, 65])
     
+    # no set_wardrobe because classifyNew() will handle appending items to wardrobe
+    def test_wardrobe(self):
+        newUser = User(" ")
+        self.assertEqual(newUser.get_wardrobe(), [])
+
+    # test that classifyNew correctly adds a clothing item to user's wardrobe
+    def test_classifyNew(self):
+        newUser = User(" ")
+        self.assertEqual(newUser.get_wardrobe(), [])
+        test_img = "gs://first-bucket-example/t-shirt.jpg" # google vision api takes imageURI from cloud storage bucket
+        newUser.classifyNew(test_img)
+        test_item = Clothing("t-shirt", test_img, 0)
+        updated_wardrobe = newUser.get_wardrobe()
+        self.assertEqual(updated_wardrobe, [test_item])
+
+class TestClothing(unittest.TestCase):
+
+    def test_objectName(self):
+        newClothing = Clothing("t-shirt", "gs://first-bucket-example/t-shirt.jpg", 0)
+        self.assertEqual(newClothing.get_objectName(), "t-shirt")
+        self.assertTrue(newClothing.set_objectName("t-shirt"))
+        self.assertFalse(newClothing.set_objectName(""), "objectName is empty") 
+        self.assertFalse(newClothing.set_objectName("123456"), "objectName must have at least one letter")
+        self.assertFalse(newClothing.set_objectName(0), "objectName must be type string") 
+
+    def test_warmthRating(self):
+        newClothing = Clothing("t-shirt", "gs://first-bucket-example/t-shirt.jpg", 0)
+        self.assertEqual(newClothing.get_warmthRating(), 0)
+        self.assertTrue(newClothing.set_warmthRating(0))
+        self.assertFalse(newClothing.set_warmthRating("0"), "warmthRating must be type int")
+
+    def test_classification(self):
+        newClothing = Clothing("t-shirt", "gs://first-bucket-example/t-shirt.jpg", 0)
+        self.assertTrue(newClothing.set_classification("top"))
+        self.assertFalse(newClothing.set_classification(""), "classification cannot be empty")
+        self.assertFalse(newClothing.set_classification(0), "classification must be type string")
+        self.assertFalse(newClothing.set_classification("1"), "classification cannot have numbers")
+        self.assertFalse(newClothing.set_classification("top!"), "classification cannot have special characters")
+        newClothing.set_classification("top")
+        self.assertEqual(newClothing.get_classification(), "top")
+
 if __name__ == '__main__':
         unittest.main() 
